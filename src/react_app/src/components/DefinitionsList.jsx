28253e1af8a7d0bd29be61aad1fa3e1036873925
Ula-Lysenko <highspirit@ukr.net>
2024-04-29 17:22:03 +0300

    hw 86 completed

---
+++ src/react_app/src/components/DefinitionsList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class DefinitionsList extends React.Component {
+    render() {
+        const { data } = this.props
+
+        return (
+            <dl>
+                {data.map(item => (
+                 <React.Fragment key={item.id}>
+                        <dt>{item.dt}</dt>
+                        <dd>{item.dd}</dd>
+                  </React.Fragment>
+                ))}
+            </dl>
+
+        );
+    }
+}
+
+export default DefinitionsList;
