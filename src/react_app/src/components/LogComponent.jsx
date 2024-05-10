21670d94c4015a7c866aba7ab365bc183b002fcc
Ula-Lysenko <highspirit@ukr.net>
2024-05-10 20:26:58 +0300

    hw 96 completed

---
+++ src/react_app/src/components/LogComponent.jsx
@@ -0,0 +1,61 @@
+import React, {Component} from "react";
+
+class LogComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            log: [],
+        }
+    }
+
+    handleAddClick = () => {
+        const lastValue = this.state.log.length > 0 ? this.state.log[0] : 0;
+        const newValue = lastValue + 1;
+
+        this.setState((prevState) => ({
+            log: [newValue, ...prevState.log],
+        }));
+    }
+
+    handleSubClick = () => {
+        const lastValue = this.state.log.length > 0 ? this.state.log[0] : 0;
+        const newValue = lastValue - 1;
+
+        this.setState((prevState) => ({
+            log: [newValue, ...prevState.log],
+        }));
+
+    }
+
+    handleRemoveClick = (index) => {
+        this.setState((prevState) => ({
+            log: prevState.log.filter((_, i) => i !== index),
+        }));
+    }
+
+    render() {
+
+        return(
+            <div>
+                <div className='btn-group font-monospace' role='group'>
+                    <button type='button' className='btn-outline-success' onClick={this.handleAddClick}>
+                        +
+                    </button>
+                    <button type='button' className='btn-outline-danger' onClick={this.handleSubClick}>
+                        -
+                    </button>
+                </div>
+                <div className='list-group'>
+                    {this.state.log.map((value, index) => (
+                        <button key={index} type='button' className="list-group-item list-group-item-action" onClick={() => this.handleRemoveClick(index)} >
+                            {value}
+                        </button>
+                    ))}
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default LogComponent;
