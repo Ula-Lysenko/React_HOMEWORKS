25eeca3adc7b4f77cccabbf10d0716f1d6fd4de5
Ula-Lysenko <highspirit@ukr.net>
2024-05-28 17:23:55 +0300

    hw 102 updated

--- src/react_app/src/main.jsx
+++ src/react_app/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import '@toast-ui/editor/dist/toastui-editor.css';
@@ -10,6 +9,6 @@ import MarkdownEditor from "./components/MarkdownEditor.jsx";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <MarkdownEditor onContentChange={console.log} />
-      10 Celebrities Whose Children Have Ordinary Jobs
+      10 Celebrities Whose Children Have Ordinary Jobs.
   </React.StrictMode>,
 );
